42505a2000dac8fa80923fb41b770b3180eb86f3
AntonyHop <stargeit123@gmail.com>
2019-05-16 00:46:56 +0300

    delete fucking gitignore and yarik bochok

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php 
+define('CONTROLLER', 'controller');
+define('CONTROLLER_PREFIX', '_con');
+define('MODEL', 'model');
+define('MODEL_PREFIX', '_mod');
+
+
+
+define('HOME_CONTROL', 'main_con');
+define('DBHOST', 'localhost');
+
+
+define('DB_USERNAME', 'root');
+define('DB_PASSWORT', '');
+define('DB_NAME', 'phplab');
+
+
+
+
